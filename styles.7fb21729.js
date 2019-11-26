3361354414cc389c8d654060da4f540f0434b457
Leonid Feskov <leonik89@gmail.com>
2019-11-26 20:41:29 +0300

    Updates

--- styles.7fb21729.js
+++ styles.7fb21729.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./blocks/header/search-icon.svg":[["search-icon.2c36ba85.svg","css/blocks/header/search-icon.svg"],"css/blocks/header/search-icon.svg"],"./blocks/header/login-icon.svg":[["login-icon.68986608.svg","css/blocks/header/login-icon.svg"],"css/blocks/header/login-icon.svg"],"./blocks/header/menu-icon.svg":[["menu-icon.d0bcd458.svg","css/blocks/header/menu-icon.svg"],"css/blocks/header/menu-icon.svg"],"_css_loader":"../../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60461" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63237" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
