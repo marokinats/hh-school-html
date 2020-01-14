b1336a9450677734f95104b9d6ebecd990a903dd
Tania Marokina <tmarokina@synergy.ru>
2020-01-14 11:37:37 +0300

    Updates

--- js.00a46daa.js
+++ js.00a46daa.js
@@ -428,6 +428,8 @@ inputAreas.addEventListener('input', function (e) {
       suggestions = cities['items'];
       showSuggestions(suggestions);
     });
+  } else {
+    suggestionsWrapper.style.display = 'none';
   }
 });
 suggestionsWrapper.addEventListener('click', function (e) {
@@ -521,7 +523,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59432" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60691" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
